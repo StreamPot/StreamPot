4f18f2b9f2bd7208e69e7e0cbd777772255d2569
Ostap Brehin <osbre@protonmail.com>
2024-04-08 13:42:14 +0100

    Remove unused

--- packages/server/src/services/video.ts
+++
@@ -1,9 +0,0 @@
-export function trim({
-    sourcePath,
-    outputPath,
-    timeline: { start, end },
-}) {
-    console.log("trimming video")
-    console.log(sourcePath, outputPath, start, end)
-    return Promise.resolve()
-}
