82ae9072fe7756a977715ed7d89c11fa3b2e4ef8
Ostap Brehin <osbre@protonmail.com>
2024-05-07 00:07:05 +0100

    Update comment

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -2,8 +2,7 @@ import StreamPot from './StreamPot';
 import { StreamPotOptions } from "./types";
 
 /**
- * The purpose of this class is to replace itself with a new instance of StreamPot.
- * This is done to allow method chaining without having to instantiate a new StreamPot instance every time (user convenience)
+ * For user's convenience, this class returns a new StreamPot instance after the first method call.
  */
 export default class StreamPotFactory extends StreamPot {
     constructor(options: StreamPotOptions) {
@@ -25,7 +24,7 @@ class StreamPotProxyHandler implements ProxyHandler<StreamPotFactory> {
         const instance = new StreamPot(this.options);
 
         // @ts-ignore
-        // Call the method on this specific class instance
+        // Call the method on the specific class instance
         return (instance)[prop].bind(instance);
     }
 }
