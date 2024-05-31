b291c595e35935da7bd1eb8d5fc8288aa006938b
Jack Bridger <jacksbridger@gmail.com>
2024-05-31 16:58:04 +0100

    Add posthog to docs

--- packages/landing/.vitepress/theme/index.ts
+++ packages/landing/.vitepress/theme/index.ts
@@ -1,5 +1,6 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
+import { h, watch } from 'vue'
+import { useRoute } from 'vitepress';
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
@@ -7,6 +8,28 @@ import CodeExample from './CodeExample.vue'
 
 export default {
     extends: DefaultTheme,
+    setup() {
+        const route = useRoute();
+        watch(
+            () => route.path,
+            (to, from) => {
+                window.posthog?.capture(
+                    '$pageleave',
+                    {
+                        to: to,
+                        $current_url: from
+                    }
+                )
+                window.posthog?.capture(
+                    '$pageview',
+                    {
+                        from: from,
+                        $current_url: to
+                    }
+                )
+            },
+        );
+    },
     Layout: () => {
         return h(DefaultTheme.Layout, null, {
             // https://vitepress.dev/guide/extending-default-theme#layout-slots
